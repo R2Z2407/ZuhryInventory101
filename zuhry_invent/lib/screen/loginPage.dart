3573afe347bcfc9f4fc8528149a0a5edf4850eb3
R2Z2407 <90142366+R2Z2407@users.noreply.github.com>
2023-11-29 23:56:26 +0700

    Update loginPage.dart

--- zuhry_invent/lib/screen/loginPage.dart
+++ zuhry_invent/lib/screen/loginPage.dart
@@ -24,96 +24,89 @@ class _LoginPageState extends State<LoginPage> {
           elevation: 0,
         ), //appbar
         body: Container(
-          child: SingleChildScrollView(
-              padding: const EdgeInsets.all(32),
-              child: Stack(
-                children: [
-                  // Positioned(
-                  //     right: 0,
-                  //     top: 0,
-                  //     width: 150,
-                  //     child: Container(
-                  //       child: Image.asset('assets/svg/Atas Kanan.png'),
-                  //     )),
-                  // Positioned(
-                  //     left: 0,
-                  //     top: 0,
-                  //     width: 260,
-                  //     child: Container(
-                  //       child: Image.asset('assets/svg/Atas Kiri.png'),
-                  //     )),
-                  Column(
-                    children: [
-                      const Text(
-                        'Log In',
-                        style: TextStyle(
-                            fontFamily: 'Nunito',
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      const TextField(
-                        decoration: InputDecoration(
-                            label: Text(
-                              'NIP',
-                              style: TextStyle(
-                                  fontFamily: 'Nunito',
-                                  fontWeight: FontWeight.w400),
-                            ),
-                            prefixIcon: Icon(Icons.person)),
-                      ),
-                      TextField(
-                        obscureText: is_onpress,
-                        decoration: InputDecoration(
-                          label: const Text(
-                            'Password',
-                            style: TextStyle(
-                                fontFamily: 'Nunito',
-                                fontWeight: FontWeight.w400),
-                          ),
-                          prefixIcon: Icon(Icons.lock),
-                          suffixIcon: IconButton(
-                              icon: const Icon(Icons.remove_red_eye),
-                              onPressed: () {
-                                setState(() {
-                                  is_onpress = !is_onpress;
-                                });
-                              }),
-                        ),
-                      ),
-                      const SizedBox(height: 25),
-                      CustomButton(
-                        text: 'Log In',
-                        onPressed: () => Navigator.popAndPushNamed(
-                            context, HomePage.routeName),
-                        height: 40,
-                        width: 300,
-                      ),
-                      const SizedBox(height: 30),
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text(
-                              'Dont have an account?',
-                              style: TextStyle(
-                                  fontFamily: 'Nunito',
-                                  fontWeight: FontWeight.w400),
-                            ),
-                            TextButton(
-                                onPressed: () {
-                                  Navigator.pushNamed(
-                                      context, SignUpPage.routeName);
-                                },
-                                child: const Text(
-                                  'Sign Up',
-                                  style: TextStyle(
-                                      fontFamily: 'Nunito',
-                                      color: Coolors.primary),
-                                ))
-                          ])
-                    ],
-                  )
-                ],
+          child: Stack(
+        children: [
+          Positioned(
+              right: 0,
+              top: 0,
+              width: 150,
+              child: Container(
+                child: Image.asset('assets/svg/Atas Kanan.png'),
               )),
-        ));
+          Positioned(
+              left: 0,
+              top: 0,
+              width: 260,
+              child: Container(
+                child: Image.asset('assets/svg/Atas Kiri.png'),
+              )),
+          SingleChildScrollView(
+              padding: const EdgeInsets.all(32),
+              child: Column(
+                children: [
+                  const Text(
+                    'Log In',
+                    style: TextStyle(
+                        fontFamily: 'Nunito',
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(
+                        label: Text(
+                          'NIP',
+                          style: TextStyle(
+                              fontFamily: 'Nunito',
+                              fontWeight: FontWeight.w400),
+                        ),
+                        prefixIcon: Icon(Icons.person)),
+                  ),
+                  TextField(
+                    obscureText: is_onpress,
+                    decoration: InputDecoration(
+                      label: const Text(
+                        'Password',
+                        style: TextStyle(
+                            fontFamily: 'Nunito', fontWeight: FontWeight.w400),
+                      ),
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: IconButton(
+                          icon: const Icon(Icons.remove_red_eye),
+                          onPressed: () {
+                            setState(() {
+                              is_onpress = !is_onpress;
+                            });
+                          }),
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  CustomButton(
+                    text: 'Log In',
+                    onPressed: () =>
+                        Navigator.popAndPushNamed(context, HomePage.routeName),
+                    height: 40,
+                    width: 300,
+                  ),
+                  const SizedBox(height: 30),
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    const Text(
+                      'Dont have an account?',
+                      style: TextStyle(
+                          fontFamily: 'Nunito', fontWeight: FontWeight.w400),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, SignUpPage.routeName);
+                        },
+                        child: const Text(
+                          'Sign Up',
+                          style: TextStyle(
+                              fontFamily: 'Nunito', color: Coolors.primary),
+                        ))
+                  ])
+                ],
+              ))
+        ],
+      )),);
   }
 }
