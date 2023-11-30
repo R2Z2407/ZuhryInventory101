5cbca3da2fd34e9e3b4ddcdede22ec25c5985be2
R2Z2407 <90142366+R2Z2407@users.noreply.github.com>
2023-11-30 19:29:55 +0700

    Update signUpPage.dart

--- zuhry_invent/lib/screen/signUpPage.dart
+++ zuhry_invent/lib/screen/signUpPage.dart
@@ -11,95 +11,136 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
-  TextEditingController _nipController = TextEditingController();
-  TextEditingController _nameController = TextEditingController();
-  // TextEditingController _passwordController = TextEditingController();
   late bool is_onpress = true;
 
   @override
   Widget build(BuildContext context) {
+    final deviceHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.all(32),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Sign Up',
-                style: TextStyle(
-                    fontFamily: 'Nunito',
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 32),
-              TextField(
-                controller: _nipController,
-                decoration: InputDecoration(
-                  label: const Text(
-                    'NIP',
-                    style: TextStyle(
-                        fontFamily: 'Nunito', fontWeight: FontWeight.w400),
-                  ),
-                ),
-              ),
-              SizedBox(height: 16),
-              TextField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                  label: const Text(
-                    'Name',
-                    style: TextStyle(
-                        fontFamily: 'Nunito', fontWeight: FontWeight.w400),
-                  ),
-                ),
-              ),
-              SizedBox(height: 16),
-              TextField(
-                obscureText: is_onpress,
-                decoration: InputDecoration(
-                  label: const Text(
-                    'Password',
-                    style: TextStyle(
-                        fontFamily: 'Nunito', fontWeight: FontWeight.w400),
-                  ),
-                  suffixIcon: IconButton(
-                      icon: const Icon(Icons.remove_red_eye),
-                      onPressed: () {
-                        setState(() {
-                          is_onpress = !is_onpress;
-                        });
-                      }),
-                ),
-              ),
-              SizedBox(height: 32),
-              CustomButton(
-                text: 'Sign Up',
-                onPressed: () =>
-                    Navigator.pushNamed(context, LoginPage.routeName),
-                height: 40,
-                width: 300,
-              ),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(width: 3),
-                  Text('Already have an account?'),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.pop(context, LoginPage.routeName);
-                      },
-                      child: const Text(
-                        'Login',
-                        style: TextStyle(color: Coolors.primary),
+        backgroundColor: Coolors.primary,
+        body: Container(
+            child: Stack(children: [
+          Positioned(
+              right: 0,
+              top: 0,
+              width: 150,
+              child: Container(
+                child: Image.asset('assets/svg/Atas Kanan.png'),
+              )),
+          Positioned(
+              left: 0,
+              top: 0,
+              width: 260,
+              child: Container(
+                child: Image.asset('assets/svg/Atas Kiri.png'),
+              )),
+          SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: deviceHeight * 0.23,
+                  padding: EdgeInsets.all(15),
+                  alignment: Alignment.centerRight,
+                  child: Text('Zuhry Inventory',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 32,
+                        fontFamily: 'Nunito',
+                        fontWeight: FontWeight.w700,
+                        height: 0,
                       )),
-                ],
-              )
-            ],
+                ),
+                Container(
+                    height: deviceHeight * 0.17,
+                    padding: EdgeInsets.all(30),
+                    alignment: Alignment.bottomLeft,
+                    child: Text('Sign Up',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontFamily: 'Nunito',
+                          fontWeight: FontWeight.w400,
+                          height: 0,
+                        ))),
+                Container(
+                  height: deviceHeight * 0.6,
+                  padding: const EdgeInsets.all(32),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(60),
+                      )),
+                  child: Column(
+                    children: [
+                      const TextField(
+                        decoration: InputDecoration(
+                          label: const Text(
+                            'Name',
+                            style: TextStyle(
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ),
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                          label: const Text(
+                            'NIP',
+                            style: TextStyle(
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ),
+                      ),
+                      TextField(
+                        obscureText: is_onpress,
+                        decoration: InputDecoration(
+                          label: const Text(
+                            'Password',
+                            style: TextStyle(
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w400),
+                          ),
+                          suffixIcon: IconButton(
+                              icon: const Icon(Icons.remove_red_eye),
+                              onPressed: () {
+                                setState(() {
+                                  is_onpress = !is_onpress;
+                                });
+                              }),
+                        ),
+                      ),
+                      SizedBox(height: 32),
+                      CustomButton(
+                        text: 'Sign Up',
+                        onPressed: () =>
+                            Navigator.pushNamed(context, LoginPage.routeName),
+                        height: 40,
+                        width: 300,
+                      ),
+                      SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(width: 3),
+                          Text('Already have an account?'),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.pop(context, LoginPage.routeName);
+                              },
+                              child: const Text(
+                                'Login',
+                                style: TextStyle(color: Coolors.primary),
+                              )),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-      ),
-    );
+        ])));
   }
 }
