a733524d4737962f8ebfbfbcabfff7506825ce25
R2Z2407 <90142366+R2Z2407@users.noreply.github.com>
2023-12-01 20:37:23 +0700

    Update detailPage.dart

--- zuhry_invent/lib/screen/detailPage.dart
+++ zuhry_invent/lib/screen/detailPage.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:zuhry_invent/screen/homePage.dart';
 import 'package:zuhry_invent/common/utils/custom_colors.dart';
 
-class DetailPage extends StatelessWidget {
+class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
-  static String routeName = "/detailpage";
+  static String routeName = "/DetailPage";
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  late bool is_onpress = true;
 
   @override
   Widget build(BuildContext context) {
@@ -134,6 +141,143 @@ class DetailPage extends StatelessWidget {
               ),
             ],
           ),
-        ]));
+        ]),
+        bottomNavigationBar: Container(
+            width: MediaQuery.of(context).size.width,
+            color: Coolors.primary,
+            height: 60,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  //Kembali
+                  height: 60,
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  color: Coolors.third,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (Context) => HomePage()));
+                    },
+                    child: Ink(
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Kembali",
+                          style: TextStyle(
+                              fontFamily: "Nurito",
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                              color: Coolors.primary),
+                          textAlign: TextAlign.justify,
+                        ),
+                      ),
+                      height: 60,
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        border: Border.all(
+                          color: Coolors.third,
+                          width: 1,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  //Kembali
+                  height: 60,
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  color: Coolors.primary,
+                  child: InkWell(
+                    onTap: () {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return Container(
+                            padding: EdgeInsets.all(15),
+                            color: Colors.white,
+                            height: 203,
+                            child: Column(children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    height: 60,
+                                    width: 60,
+                                    color: Colors.grey,
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text("Nama Barang"),
+                                  Container()
+                                ],
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              TextField(
+                                  decoration: InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      hintText:
+                                          "Ex: Tugas Akhir, Praktikum, Proyek",
+                                      label: Text(
+                                        'Keterangan Keperluan',
+                                        style: TextStyle(
+                                            fontFamily: 'Nunito',
+                                            fontWeight: FontWeight.w500),
+                                      ))),
+                              SizedBox(height: 10),
+                              Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Container(
+                                      width: 100,
+                                      height: 40,
+                                      alignment: Alignment.center,
+                                      decoration: BoxDecoration(
+                                        color: Coolors.primary,
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      child: ElevatedButton(onPressed: , child: Text(
+                                        "Pinjam",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontFamily: "Nunito",
+                                            fontSize: 16),
+                                      ),)
+                                    ),
+                                  ])
+                            ]),
+                          );
+                        },
+                      );
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Pinjam",
+                        style: TextStyle(
+                            fontFamily: "Nurito",
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                            color: Coolors.third),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                    //height: 60,
+                    //width: MediaQuery.of(context).size.width * 0.5,
+                    //decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    // border: Border.all(
+                    // color: Coolors.third,
+                    // width: 1,
+                  ),
+                ),
+                // ),
+                //),
+              ],
+            )));
   }
 }
