ab42f5ca3c6003899ac38f35916d468481c55e5c
R2Z2407 <90142366+R2Z2407@users.noreply.github.com>
2023-12-02 20:18:59 +0700

    Update galeryPage.dart

--- zuhry_invent/lib/screen/galeryPage.dart
+++ zuhry_invent/lib/screen/galeryPage.dart
@@ -1,9 +1,18 @@
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:zuhry_invent/screen/detailPage.dart';
 import 'package:zuhry_invent/common/utils/custom_colors.dart';
 
+
 class GaleryPage extends StatelessWidget {
-  const GaleryPage({super.key});
+  final String image;
+  final String Subject;
+  final Int Quantity;
+  const GaleryPage(
+      {super.key,
+      required this.image,
+      required this.Subject,
+      required this.Quantity});
 
   @override
   Widget build(BuildContext context) {
@@ -20,15 +29,28 @@ class GaleryPage extends StatelessWidget {
                   .push(MaterialPageRoute(builder: (Context) => DetailPage()));
             },
             child: Ink(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(5),
-                border: Border.all(
-                  color: Coolors.second,
-                  width: 1,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                  border: Border.all(
+                    color: Coolors.second,
+                    width: 1,
+                  ),
                 ),
-              ),
-            ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height*0.5,
+                      child: Image.asset(image),
+                    ),
+                    Text(
+                      Subject, style: TextStyle(fontFamily: 'Nunito', fontSize: 14, fontWeight: FontWeight.w700, color: Coolors.second
+                    )
+                    ),
+                    Text("Tersedia dalam $Quantity", style: TextStyle(fontFamily: 'Nunito', fontSize: 14, fontWeight: FontWeight.w700, color: Coolors.second
+                    ) )
+                  ],
+                )),
           ),
         ),
       );
