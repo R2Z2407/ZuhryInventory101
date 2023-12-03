0481b8bcb5c04643b62111b5acef962c7ced1f3d
R2Z2407 <90142366+R2Z2407@users.noreply.github.com>
2023-12-03 09:25:16 +0700

    Update ListLoan.dart

--- zuhry_invent/lib/screen/ListLoan.dart
+++ zuhry_invent/lib/screen/ListLoan.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:zuhry_invent/component/ListviewSquare_LL.dart';
 
 class Loanlist extends StatefulWidget {
   const Loanlist({super.key});
@@ -11,8 +12,23 @@ class Loanlist extends StatefulWidget {
 class _LoanlistState extends State<Loanlist> {
   @override
   Widget build(BuildContext context) {
+    final List _LoanList = [
+      'History 1',
+      'History 2',
+      'History 3',
+      'History 4',
+      'History 5',
+      'History 6',
+      'History 7',
+    ];
     return Scaffold(
-      appBar: AppBar(title: Text("Loans")),
+      appBar: AppBar(title: Text("Daftar Barang")),
+      body: SingleChildScrollView(
+          child: ListView.builder(
+              itemCount: _LoanList.length,
+              itemBuilder: (context, index) {
+                return MySquare_LL(child: _LoanList[index]);
+              })),
     );
   }
 }
