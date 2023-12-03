4b3538f992a01fd71a347057dcf5882a6f55c196
R2Z2407 <90142366+R2Z2407@users.noreply.github.com>
2023-12-03 09:27:18 +0700

    Update galeryPage.dart

--- zuhry_invent/lib/screen/galeryPage.dart
+++ zuhry_invent/lib/screen/galeryPage.dart
@@ -1,18 +1,11 @@
-import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:zuhry_invent/screen/detailPage.dart';
 import 'package:zuhry_invent/common/utils/custom_colors.dart';
 
-
 class GaleryPage extends StatelessWidget {
   final String image;
   final String Subject;
-  final Int Quantity;
-  const GaleryPage(
-      {super.key,
-      required this.image,
-      required this.Subject,
-      required this.Quantity});
+  const GaleryPage({super.key, required this.image, required this.Subject});
 
   @override
   Widget build(BuildContext context) {
@@ -40,15 +33,15 @@ class GaleryPage extends StatelessWidget {
                 child: Column(
                   children: [
                     Container(
-                      height: MediaQuery.of(context).size.height*0.5,
+                      height: 50,
                       child: Image.asset(image),
                     ),
-                    Text(
-                      Subject, style: TextStyle(fontFamily: 'Nunito', fontSize: 14, fontWeight: FontWeight.w700, color: Coolors.second
-                    )
-                    ),
-                    Text("Tersedia dalam $Quantity", style: TextStyle(fontFamily: 'Nunito', fontSize: 14, fontWeight: FontWeight.w700, color: Coolors.second
-                    ) )
+                    Text(Subject,
+                        style: TextStyle(
+                            fontFamily: 'Nunito',
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700,
+                            color: Coolors.second))
                   ],
                 )),
           ),
